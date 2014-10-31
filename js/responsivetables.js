befebbae456e8bcae4a5990ccf8c59b65c634e44
Kevin Thornbloom <kevin@kthornbloom.com>
2014-10-31 16:29:46 -0400

    Adding debounced resize function

--- js/responsivetables.js
+++ js/responsivetables.js
@@ -60,9 +60,38 @@
 
             // Call on page load and page resize
             tableChecker();
-            $(window).resize(function () {
-                tableChecker();
+
+            (function($,sr){
+              var debounce = function (func, threshold, execAsap) {
+                  var timeout;
+
+                  return function debounced () {
+                      var obj = this, args = arguments;
+                      function delayed () {
+                          if (!execAsap)
+                              func.apply(obj, args);
+                          timeout = null;
+                      };
+
+                      if (timeout)
+                          clearTimeout(timeout);
+                      else if (execAsap)
+                          func.apply(obj, args);
+
+                      timeout = setTimeout(delayed, threshold || 100);
+                  };
+              }
+              // smartresize 
+              jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+            })(jQuery,'smartresize');
+
+
+            // On resize (with delay)
+            $(window).smartresize(function(){
+              tableChecker();
             });
+
 
         }
     });
